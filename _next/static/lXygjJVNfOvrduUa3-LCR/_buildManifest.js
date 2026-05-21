839ddb0f618d9943b4fd7bd767fec096a0f70f23
Ramkumar143z <ramcse142@gmail.com>
2026-05-21 18:24:57 +0530

    Updates

---
+++ _next/static/lXygjJVNfOvrduUa3-LCR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PORTFOLIO//_next/:path+",
+        "destination": "/PORTFOLIO/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
